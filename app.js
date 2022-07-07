61e2dc6d4742bd906ae6ad6057fad79ff0fb5d4a
AlexTeper99 <ateper@fi.uba.ar>
2022-07-07 03:32:44 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+//Importaciones
+const express = require('express')
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+require('dotenv').config()
+
+//const { Student } = require('./src/db/models');
+
+app.get('/hola', async function(req, res){
+    res.status(201).send('Hola');
+})
+
+
+
+
+//Definir el puerto de escucha
+app.listen(process.env.PORT)
